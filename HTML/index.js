3adc31e8a85fefbda7783b9f7c04d18889dbc33d
slansky98 <slansky98@gmail.com>
2025-09-20 15:43:14 -0500

    Tuesday works

--- HTML/index.js
+++ HTML/index.js
@@ -1,13 +1,60 @@
-function createSomething(buttonName){ //Hopefully a clock
-    document.querySelector(buttonName).addEventListener('click', alert("WAKE THE FUCK UP"));
+function displayWorkout(buttonName, exercises) {
+    const tuesdayExercises = [
+        "Pushups - 45 seconds",
+        "Rest - 20 seconds",
+        "Squats Knee Drive - 45 seconds",
+        "Rest - 20 seconds",
+        "Glute bridge - 45 seconds",
+        "Rest - 20 seconds",
+        "Superman - 45 seconds",
+        "Rest - 20 seconds",
+        "Plank shoulder taps - 45 seconds"
+    ]
+    
+    const workoutDisplay = document.getElementById("workoutDisplay");
+
+    document.getElementById("tuesWorkout").addEventListener('click', 
+    function(){
+        var tueUl = document.createElement("ul");
+        for (let i = 0; i < tuesdayExercises.length; i++) {
+            var tueLi = document.createElement("li");
+            tueLi.textContent = tuesdayExercises[i];
+            tueUl.appendChild(tueLi);  
+        }
+        workoutDisplay.appendChild(tueUl);
+        document.getElementById("tuesWorkout").disabled = true;
+    });
+    
 }
 
-function tuesdayWorkout() {
-    document.getElementById("tuesWorkout").addEventListener('click', 
-    function() {
-    alert('Button was clicked!');}
-);;
 
+function displayWorkout(buttonName, exercises) {
+    const tuesdayExercises = [
+        "Pushups - 45 seconds",
+        "Rest - 20 seconds",
+        "Squats Knee Drive - 45 seconds",
+        "Rest - 20 seconds",
+        "Glute bridge - 45 seconds",
+        "Rest - 20 seconds",
+        "Superman - 45 seconds",
+        "Rest - 20 seconds",
+        "Plank shoulder taps - 45 seconds"
+    ]
+    
+    const workoutDisplay = document.getElementById("workoutDisplay");
+
+    document.getElementById("tuesWorkout").addEventListener('click', 
+    function(){
+        var tueUl = document.createElement("ul");
+        for (let i = 0; i < tuesdayExercises.length; i++) {
+            var tueLi = document.createElement("li");
+            tueLi.textContent = tuesdayExercises[i];
+            tueUl.appendChild(tueLi);  
+        }
+        workoutDisplay.appendChild(tueUl);
+        document.getElementById("tuesWorkout").disabled = true;
+    });
+    
 }
 
 
